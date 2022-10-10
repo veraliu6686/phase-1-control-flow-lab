95debe95c5c51c17d1939a7768e6bc9314e325cf
lianzi liu <lianziliu.sugar@gmail.com>
2022-10-10 16:16:05 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(i){
   // Write your code here!
+  if (i > 2500){
+    return 'No can do.';
+  } else if (i > 2000) {
+      return 'I will gladly take your thirty bucks.' ;
+  } else if (i > 400 && i < 2000) {
+      return 'That will be twenty bucks.';
+  } else{
+      return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+
+    case "not as generous":
+      return "Thank you.";
+
+    default:
+      return "Bye."
+  }
 }
